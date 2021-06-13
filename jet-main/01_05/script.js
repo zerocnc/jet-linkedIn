95fcdea25390264dba52f6e22cd1e3c152952e05
zerocnc <gdi_richard@yahoo.com>
2021-06-13 01:57:41 -0700

    Follow examples with crouse by changing script tags to reflect current standards.

--- jet-main/01_05/script.js
+++ jet-main/01_05/script.js
@@ -7,6 +7,11 @@
 /* Multi-line comment
 See! this line is also commented out! */
 
+/**
+ * function updateBackpack()
+ * Outputs HTML
+ * @param {string} update
+ */
 const updateBackpack = (update) => {
   let main = document.querySelector("main"); // main is an element
   main.innerHTML = markup(backpack);
